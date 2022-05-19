2e9bb8876a05a48dd474fb6ca7ca7ee75f7bfe0c
DevExpressExampleBot <fakeEmail>
2022-05-19 11:13:52 +0400

    Source auto update [skip ci]

--- VB/DxSample/Form1.Designer.vb
+++ VB/DxSample/Form1.Designer.vb
@@ -1,78 +1,79 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace DxSample
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
             Me.btnSave = New System.Windows.Forms.Button()
             Me.btnAllowUpdate = New System.Windows.Forms.Button()
             Me.btnRestore = New System.Windows.Forms.Button()
             Me.pivotGrid1 = New DxSample.MyPivotGrid()
-            CType(Me.pivotGrid1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.pivotGrid1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
-            '
-            'btnSave
-            '
+            ' 
+            ' btnSave
+            ' 
             Me.btnSave.Location = New System.Drawing.Point(12, 12)
             Me.btnSave.Name = "btnSave"
             Me.btnSave.Size = New System.Drawing.Size(75, 23)
             Me.btnSave.TabIndex = 1
             Me.btnSave.Text = "Save"
             Me.btnSave.UseVisualStyleBackColor = True
-            '
-            'btnAllowUpdate
-            '
+            AddHandler Me.btnSave.Click, New System.EventHandler(AddressOf Me.btnSave_Click)
+            ' 
+            ' btnAllowUpdate
+            ' 
             Me.btnAllowUpdate.Location = New System.Drawing.Point(93, 12)
             Me.btnAllowUpdate.Name = "btnAllowUpdate"
             Me.btnAllowUpdate.Size = New System.Drawing.Size(75, 23)
             Me.btnAllowUpdate.TabIndex = 2
             Me.btnAllowUpdate.Text = "Update"
             Me.btnAllowUpdate.UseVisualStyleBackColor = True
-            '
-            'btnRestore
-            '
+            AddHandler Me.btnAllowUpdate.Click, New System.EventHandler(AddressOf Me.btnUpdate_Click)
+            ' 
+            ' btnRestore
+            ' 
             Me.btnRestore.Location = New System.Drawing.Point(174, 12)
             Me.btnRestore.Name = "btnRestore"
             Me.btnRestore.Size = New System.Drawing.Size(75, 23)
             Me.btnRestore.TabIndex = 3
             Me.btnRestore.Text = "Restore"
             Me.btnRestore.UseVisualStyleBackColor = True
-            '
-            'pivotGrid1
-            '
-            Me.pivotGrid1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            AddHandler Me.btnRestore.Click, New System.EventHandler(AddressOf Me.btnRestore_Click)
+            ' 
+            ' pivotGrid1
+            ' 
+            Me.pivotGrid1.Anchor = CType(((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
             Me.pivotGrid1.Location = New System.Drawing.Point(12, 41)
             Me.pivotGrid1.Name = "pivotGrid1"
             Me.pivotGrid1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized
             Me.pivotGrid1.Size = New System.Drawing.Size(698, 359)
             Me.pivotGrid1.TabIndex = 0
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(722, 412)
             Me.Controls.Add(Me.btnRestore)
@@ -81,18 +82,17 @@ Namespace DxSample
             Me.Controls.Add(Me.pivotGrid1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(Me.pivotGrid1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.pivotGrid1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
 #End Region
+        Private pivotGrid1 As DxSample.MyPivotGrid
 
-        Private pivotGrid1 As MyPivotGrid
-		Private WithEvents btnSave As System.Windows.Forms.Button
-		Private WithEvents btnAllowUpdate As System.Windows.Forms.Button
-		Private WithEvents btnRestore As System.Windows.Forms.Button
+        Private btnSave As System.Windows.Forms.Button
 
-	End Class
+        Private btnAllowUpdate As System.Windows.Forms.Button
+
+        Private btnRestore As System.Windows.Forms.Button
+    End Class
 End Namespace
-
--- VB/DxSample/Form1.vb
+++ VB/DxSample/Form1.vb
@@ -1,60 +1,58 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports System.IO
-Imports System.Xml
 Imports DevExpress.XtraPivotGrid
 Imports DevExpress.Utils
 
 Namespace DxSample
-	Partial Public Class Form1
-		Inherits Form
-		Private Const LayoutFile As String = "layout.xml"
 
-		Public Sub New()
-			InitializeComponent()
-			pivotGrid1.DataSource = CreateTable(10)
-			pivotGrid1.RetrieveFields()
-			For i As Integer = 0 To pivotGrid1.Fields.Count - 1
-				pivotGrid1.Fields(i).Name = "field" & i.ToString()
-			Next i
-		End Sub
-		Private Function CreateTable(ByVal RowCount As Integer) As DataTable
-			Dim tbl As New DataTable()
-			tbl.Columns.Add("Name", GetType(String))
-			tbl.Columns.Add("ID", GetType(Integer))
-			tbl.Columns.Add("Number", GetType(Integer))
-			tbl.Columns.Add("Date", GetType(DateTime))
-			For i As Integer = 0 To RowCount - 1
-				tbl.Rows.Add(New Object() { String.Format("Name{0}", i), i, 3 - i, DateTime.Now.AddDays(i) })
-			Next i
-			Return tbl
-		End Function
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub btnSave_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnSave.Click
-			If File.Exists(LayoutFile) Then
-				File.Delete(LayoutFile)
-			End If
-			Using stream As New FileStream(LayoutFile, FileMode.OpenOrCreate, FileAccess.Write)
-				pivotGrid1.SaveLayoutToStream(stream)
-			End Using
-		End Sub
+        Const LayoutFile As String = "layout.xml"
 
-		Private Sub btnRestore_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnRestore.Click
-			Using stream As New FileStream(LayoutFile, FileMode.Open, FileAccess.Read)
-				pivotGrid1.RestoreLayoutFromStream(stream)
-			End Using
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            pivotGrid1.DataSource = CreateTable(10)
+            pivotGrid1.RetrieveFields()
+            For i As Integer = 0 To pivotGrid1.Fields.Count - 1
+                pivotGrid1.Fields(i).Name = "field" & i.ToString()
+            Next
+        End Sub
 
-		Private Sub btnUpdate_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnAllowUpdate.Click
-			For Each field As PivotGridField In pivotGrid1.Fields
-				field.Options.AllowDrag = DevExpress.Utils.DefaultBoolean.False
-			Next field
-		End Sub
-	End Class
+        Private Function CreateTable(ByVal RowCount As Integer) As DataTable
+            Dim tbl As DataTable = New DataTable()
+            tbl.Columns.Add("Name", GetType(String))
+            tbl.Columns.Add("ID", GetType(Integer))
+            tbl.Columns.Add("Number", GetType(Integer))
+            tbl.Columns.Add("Date", GetType(Date))
+            For i As Integer = 0 To RowCount - 1
+                tbl.Rows.Add(New Object() {String.Format("Name{0}", i), i, 3 - i, Date.Now.AddDays(i)})
+            Next
+
+            Return tbl
+        End Function
+
+        Private Sub btnSave_Click(ByVal sender As Object, ByVal e As EventArgs)
+            If File.Exists(LayoutFile) Then Call File.Delete(LayoutFile)
+            Using stream As FileStream = New FileStream(LayoutFile, FileMode.OpenOrCreate, FileAccess.Write)
+                pivotGrid1.SaveLayoutToStream(stream)
+            End Using
+        End Sub
+
+        Private Sub btnRestore_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Using stream As FileStream = New FileStream(LayoutFile, FileMode.Open, FileAccess.Read)
+                pivotGrid1.RestoreLayoutFromStream(stream)
+            End Using
+        End Sub
+
+        Private Sub btnUpdate_Click(ByVal sender As Object, ByVal e As EventArgs)
+            For Each field As PivotGridField In pivotGrid1.Fields
+                field.Options.AllowDrag = DefaultBoolean.False
+            Next
+        End Sub
+    End Class
 End Namespace
